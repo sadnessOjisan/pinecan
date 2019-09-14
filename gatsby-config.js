94c0991c5236978d67c2afa1ef1221b2e19914d0
sadness_ojisan <sadness.ojisan@gmail.com>
2019-09-14 15:43:57 +0900

    add: s-c

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `パイン缶`,
+    description: `美味しいパインが大好きサークル「パイン缶」の公式LPです。サークルのお知らせをお届けします。`,
+    author: `@sadnessOjisan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,9 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
   ],
 }
