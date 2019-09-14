5120f6a8738e0329d8f2073a04a1a44799e331b0
sadness_ojisan <sadness.ojisan@gmail.com>
2019-09-14 17:06:47 +0900

    update: about me

---
+++ src/components/newsItem.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Item = props => {
+  const { date, title, path } = props.data
+  return (
+    <Wrapper>
+      <StyledTitle to={`./${path}`}>
+        <Title>{title}</Title>
+        <span>{date}</span>
+      </StyledTitle>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  border: solid 1px #555555;
+  color: #555555;
+  border-radius: 4px;
+  padding: 0.8rem 1.2rem;
+`
+
+const Title = styled.h2``
+
+const StyledTitle = styled(Link)`
+  text-decoration: none;
+  color: #555555;
+`
+
+export default Item
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,29 +4,42 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Item from "../components/newsItem"
 
-const IndexPage = () => (
-  <Layout>
-    <Section>
-      <SEO title="Home" />
-      <Title>技術書典7で頒布しました！</Title>
-      <Books>
-        <BookCover>
-          <Image filename="kosubook" />
-        </BookCover>
-        <BookCover>
-          <Image filename="modalbook" />
-        </BookCover>
-      </Books>
-    </Section>
-    <Section>
-      <Title>ニュース</Title>
-    </Section>
-  </Layout>
-)
+const IndexPage = props => {
+  const newsEdges = props.data.allMarkdownRemark.edges
+  return (
+    <Layout>
+      <Section>
+        <SEO title="Home" />
+        <Title>技術書典7で頒布しました！</Title>
+        <Books>
+          <BookCover>
+            <Image filename="kosubook" />
+          </BookCover>
+          <BookCover>
+            <Image filename="modalbook" />
+          </BookCover>
+        </Books>
+      </Section>
+      <Section>
+        <Title>ニュース</Title>
+        {newsEdges.map(edge => (
+          <Item data={edge.node.frontmatter}></Item>
+        ))}
+      </Section>
+      <Section>
+        <Title>About Me</Title>
+        <p>連絡・質問はこちらから</p>
+        <a href="https://twitter.com/sadnessOjisan">@sadnessOjisan</a>
+      </Section>
+    </Layout>
+  )
+}
 
 const Section = styled.div`
   margin-bottom: 4.8rem;
+  font-size: 1.6rem;
 `
 
 const Title = styled.h1`
@@ -45,6 +58,22 @@ const Books = styled.div`
 const BookCover = styled.div`
   width: 40%;
   /* height: 425px; */
+`
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
 `
 
 export default IndexPage
